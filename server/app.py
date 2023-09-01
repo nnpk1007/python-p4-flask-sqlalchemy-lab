bb83cef5799192210e0f303874770bf5626319a8
Ngoc Nguyen <nnpk0410@gmail.com>
2023-09-01 11:35:45 -0500

    lab completed

--- server/app.py
+++ server/app.py
@@ -17,17 +17,94 @@ db.init_app(app)
 def home():
     return '<h1>Zoo app</h1>'
 
+# Each view should display all attributes as line items (ul). 
+# If there is a one-to-many relationship, each of the many should have its own line item.
+
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+
+    if not animal:
+        response_body = "<h1>404 animal not found</h1>"
+        response = make_response(response_body, 404)
+
+    print(animal)
+    
+    response_body = f"""
+        <ul>ID: {animal.id}</ul>
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {animal.zookeeper.name}</ul>
+        <ul>Enclosure: {animal.enclosure.environment}</ul>
+    """
+
+    response = make_response(response_body, 200)
+    
+    return response
+
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+
+    if not zookeeper:
+        response_body = "<h1>404 zookeeper not found</h1>"
+        response = make_response(response_body, 404)
+
+    # print(zookeeper.animals)
+
+    animal_names = [ animal.name for animal in zookeeper.animals]
+    print(animal_names)
+
+    response_animal_name = ""
+
+    if not animal_names:
+        response_animal_name += f"Has no animal at this time"
+    else:
+        for animal_name in animal_names:
+            response_animal_name += f"<ul>Animal: {animal_name}</ul>"  
+
+    response_body = f"""
+        <ul>ID: {zookeeper.id}</ul>
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+        <ul>{response_animal_name}</ul>
+    """
+    
+    response = make_response(response_body, 200)
+    
+    return response
+
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    environment = Enclosure.query.filter(Enclosure.id == id).first()
+
+    if not environment:
+        response_body = "<h1>404 environment not found</h1>"
+        response = make_response(response_body, 404)
+
+    animal_names = [ animal.name for animal in environment.animals]
+    print(animal_names)
+
+    response_animal_name = ""
+
+    if not animal_names:
+        response_animal_name += f"Has no animal at this time"
+    else:
+        for animal_name in animal_names:
+            response_animal_name += f"<ul>Animal: {animal_name}</ul>"  
+
+    response_body = f"""
+        <ul>ID: {environment.id}</ul>
+        <ul>Environment: {environment.environment}</ul>
+        <ul>Open to Visitors: {environment.open_to_visitors}</ul>
+        <ul>{response_animal_name}</ul>
+    """
+
+    response = make_response(response_body, 200)
+
+    return response
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,17 +7,43 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+
+# The Zookeeper model should contain a name, a birthday, and a list of animals that they take care of.
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+    animals = db.relationship("Animal", backref="zookeeper")
 
+    def __repr__(self):
+        return f"<Zookeeper {self.name}>"
+
+
+# The Enclosure model should contain an environment (grass, sand, or water), 
+# an open_to_visitors boolean, and a list of animals.
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+    animals = db.relationship("Animal", backref="enclosure")
 
+    def __repr__(self):
+        return f"<Enclosure {self.environment}>"
+
+# The Animal model should contain a name, a species, a zookeeper, and an enclosure.
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey("zookeepers.id"))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey("enclosures.id"))
+
+    def __repr__(self):
+        return f"<Animal id:{self.id} name:{self.name} species:{self.species} >"
